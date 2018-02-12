f35a766431b533002c8c47add2373a366a908478
Igor <levoshkin@gmail.com>
2018-02-12 13:51:22 +0200

    New accordion

--- functions.js
+++ functions.js
@@ -1,20 +1,45 @@
 $(document).ready(function(){
-	$('.accordion-menu li ul').css('display', 'none');
-	if($('li').children('ul')){
-		$('li').click(function(){
-			var $this = $(this);
-			var $parent = $this.parent();
-			if(!$this.children('ul').is(':visible')){
-				$this.siblings().find('ul').slideUp(500);
-				$this.siblings().find('a').find('span').html('[+]');
-				$this.children('ul').slideDown(500);
-				$this.children('a').children('span').html('[ - ]');
-			}else if($this.children('ul').is(':visible')){
-				$this.children('ul').slideUp(500);
-				$this.find('a span').html("[+]");
+    $.fn.extend({
+
+		accordion: function(options) {
+			
+			var defaults = {
+				accordion: 'true',
+				speed: 300,
+				closedSign: '[+]',
+				openedSign: '[-]'
+			};
+			
+			var opts = $.extend(defaults, options);
+			
+			$('.accordion-menu li ul').css('display', 'none');
+			if($('.accordion-menu li').children('ul'))
+				$('.accordion-menu li a span').html(opts.closedSign);
+			
+			if($('li').children('ul')){
 				
+				$('li').click(function(){
+					
+					var $this = $(this);
+					var $childUl = $this.children('ul');
+					
+					if(!$childUl.is(':visible')){
+						if(opts.accordion){
+							$this.siblings().find('ul').slideUp(opts.speed);
+							$this.siblings().find('a').find('span').html(opts.closedSign);
+						}
+						
+						$childUl.slideDown(opts.speed);
+						$this.children('a').children('span').html(opts.openedSign);
+						
+					}else if($childUl.is(':visible')){
+						$childUl.slideUp(opts.speed);
+						$this.find('a span').html(opts.closedSign);
+						
+					}
+					return false;
+				});
 			}
-			return false;
-		});
-	}
+		}
+	});
 });
--- index.php
+++ index.php
@@ -5,6 +5,18 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="/functions.js"></script>
 <link rel="stylesheet" href="/style.css">
+<script language="JavaScript">
+
+$(document).ready(function() {
+	$(".accordion-menu").accordion({
+		accordion: true,
+		speed: 700,
+		closedSign: '&#9660; ',
+		openedSign: '&#9650;'
+	});
+});
+
+</script>
 </head>
 <body>
 	<h1>MENU</h1>
